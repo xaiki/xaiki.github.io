b47aa042b644781d0bf63d6965ef40b66e2f1920
Niv Sardi <xaiki@evilgiggle.com>
2020-07-23 12:37:25 -0300

    doc id in url
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- data.js
+++ data.js
@@ -6,43 +6,59 @@ function uuidv4() {
 }
 
 const dataLinks = [
-    {source: "Anses", target: "Beneficiario", value: 10000},
+    {source: "Anses", target: "Beneficiario IFE", value: 10000},
 
-    {source: "Beneficiario", target: "Servicios Publicos", value: 3000},
-    {source: "Beneficiario", target: "Comercios Afiliado", value: 3000},
-    {source: "Beneficiario", target: "Comercios no Afiliado", value: 2000},
-    {source: "Beneficiario", target: "Banco", value: 2000},
-    /*
-       {source: "Beneficiario 2", target: "Servicios", value: 3000},
-       {source: "Beneficiario 2", target: "Comercios Afiliado", value: 3000},
-       {source: "Beneficiario 2", target: "Comercios no Afiliado", value: 2000},
-       {source: "Beneficiarios 2", target: "Banco", value: 2000},
-     */
+    {source: "Beneficiario IFE", target: "Servicios Publicos", value: 3000},
+    {source: "Beneficiario IFE", target: "Comercios Afiliado", value: 3000},
+    {source: "Beneficiario IFE", target: "Comercios no Afiliado", value: 2000},
+    {source: "Beneficiario IFE", target: "Banco", value: 2000},
+
+    {source: "Ministerio de Desarrollo Social", target: "Beneficiario IUM", value: 10000},
+
+    {source: "Beneficiario IUM", target: "Servicios Publicos", value: 3000},
+    {source: "Beneficiario IUM", target: "Comercios Afiliado", value: 3000},
+    {source: "Beneficiario IUM", target: "Comercios no Afiliado", value: 2000},
+    {source: "Beneficiario IUM", target: "Banco", value: 2000},
+
     {source: "Comercios Afiliado", target: "Proveedores Minoritarios", value: 2000},
     {source: "Comercios Afiliado", target: "Banco", value: 1000},
+    {source: "Comercios Afiliado", target: "Servicios Publicos", value: 1000},
+    {source: "Comercios Afiliado", target: "AFIP", value: 1000},
     
     {source: "Comercios no Afiliado", target: "Proveedores Minoritarios", value: 1000},
     {source: "Comercios no Afiliado", target: "Banco", value: 1000},
+    {source: "Comercios no Afiliado", target: "Servicios Publicos", value: 1000},
+    {source: "Comercios no Afiliado", target: "AFIP", value: 1000},
+
     
     {source: "Proveedores Minoritarios", target: "Proveedores Mayoritarios", value: 2000},
     {source: "Proveedores Minoritarios", target: "Servicios Publicos", value: 1000},
+
+    {source: "Ministerio de Industria", target: "Proveedores Mayoritarios", value: 2000},
+    {source: "INAES", target: "Proveedores Mayoritarios", value: 2000},
     
-    {source: "Proveedores Mayoritarios", target: "Servicios Publicos", value: 1000},
-//    {source: "Proveedores Mayoritarios", target: "Banco", value: 2000},
+    {source: "Proveedores Mayoritarios", target: "Servicios Publicos", value: 5000},
+    {source: "Proveedores Mayoritarios", target: "Banco", value: 2000},
+
+    {source: "Servicios Publicos", target: "Anses", value: 5000},
+    {source: "Servicios Publicos", target: "Ministerio de Desarrollo Social", value: 7000},
     
     {source: "Banco", target: "Proveedores Mayoritarios", value: 1000},
-//    {source: "Banco", target: "Anses", value: 5000},
+    {source: "Banco", target: "Proveedores Minoritarios", value: 1000},
+    {source: "Banco", target: "Anses", value: 2000},
+    {source: "Banco", target: "AFIP", value: 1000},
+    {source: "Banco", target: "Ministerio de Desarrollo Social", value: 3000},
 
-//    {source: "Servicios Publicos", target: "Anses", value: 5000},
+    {source: "AFIP", target: "Anses", value: 2000},
+    {source: "AFIP", target: "Ministerio de Desarrollo Social", value: 1000},
+    
+    {source: "Proveedores Mayoritarios", target: "Anses", value: 5},
+    {source: "Proveedores Mayoritarios", target: "Ministerio de Desarrollo Social", value: 5},
 ]
 
-function buildData(links) {
-    const nodes = Array.from(
-        new Set(
-            links.flatMap(l => [l.source, l.target])),
-        name => ({name, category: name.replace(/ .*/, ""), id: uuidv4()}));
-
-    return {nodes, links, units: "IFD"};
-}
 
 window.links = dataLinks
+/*
+console.error(Object.keys(dataLinks[0]).join("\t"))
+dataLinks.map(l => console.error(Object.values(l).join("\t")))
+*/
--- sankey.js
+++ sankey.js
@@ -1,167 +1,233 @@
-const {width, height} = document.querySelector('body').getClientRects()[0];
+const LINK_BASE_OPACITY = 0.9;
+const LINK_BG_OPACITY = 0.5;
+const LINK_FG_OPACITY = 1;
 
-console.error(width, height)
+const NODE_BASE_OPACITY = 0.8;
+const NODE_BG_OPACITY = 0.5;
+const NODE_FG_OPACITY = 1;
 
-const color = d3.scaleOrdinal(d3.schemeCategory10);
-const format = d3.format(',.0f');
+const uri = `https://docs.google.com/spreadsheets/d/e/${location.hash.slice(1)}/pub?output=csv`
+console.error(uri)
+d3.csv(uri)
+  .then(buildSankey)
 
-const sankey = d3.sankey()
-                 .nodeId(d => d.name)
-                 .nodeAlign(d3.sankeyJustify)
-                 .nodeWidth(15)
-                 .nodePadding(10)
-                 .extent([[1, 5], [width - 1, height - 5]]);
+function buildSankey(linkData) {
+    let {width, height} = document.querySelector('#chart').getClientRects()[0];
+    width *= 2;
 
-const makeSankey = ({nodes, links}) => sankey({
-    nodes: nodes.map(d => Object.assign({}, d)),
-    links: links.map(d => Object.assign({}, d))
-});
+    console.error(width, height)
 
-const data = buildData(window.links)
-const {nodes, links} = makeSankey(data);
+    const color = d3.scaleOrdinal([
+        ...d3.schemeCategory10,
+        "#660000", "#660066", "#666600", "#006666", "#000066",
+        "#66ffff", "#66ff66", "#6666ff", "#ff6666", "#ffff66"]);
+    const format = d3.format(',.0f');
 
-const fontScale = d3.scaleLinear() .range([10, 25]);
-fontScale.domain(d3.extent(nodes, function(d) { return d.value }));
+    function makeSankey({nodes, links}) {
+        const sankey = d3.sankeyCircular()
+                         .nodeId(d => d.name)
+                         .nodeAlign(d3.sankeyJustify)
+                         .nodeWidth(20)
+                         .nodePadding(30)
+                         .extent([[1, 5], [width - 1, height - 5]]);
+        
+        return sankey({
+            nodes: nodes.map(d => Object.assign({}, d)),
+            links: links.map(d => Object.assign({}, d))
+        })
+    };
 
-const container = d3.select('#chart');
-const svg = container.append('svg')
-                     .attr('viewBox', [0, 0, width, height]);
+    function buildData(links) {
+        console.error(links)
+        const nodes = Array.from(
+            new Set(
+                links.flatMap(l => [l.source, l.target])),
+            name => ({name, category: name.replace(/ .*/, ""), id: uuidv4()}));
 
-const nodeG = svg.append("g")
-                 .attr("stroke", "#000");
-
-const linkG = svg.append("g")
-                 .attr("fill", "none")
-                 .attr("stroke-opacity", 0.5);
-
-const nodeTextG = svg.append("g")
-                     .attr("font-family", "sans-serif")
-                     .attr("font-size", 10)
-
-function onClick(d, data) {
-    const g = document.getElementById("sliders")
-
-    if (g.configTooltip) {
-        g.configTooltip.remove();
-        g.configTooltip = null;
-        return;
+        return {nodes, links, units: "IFD"};
     }
 
-    const s = makeSlider(d.sourceLinks.reduce((a, c, i) => [...a, {
-        name: c.target.name,
-        value: c.value + (i ? a[i - 1].value : 0),
-        update: (values) => {
-            let changed = 0
-            d.sourceLinks.forEach(({index}, i) => {
-                if (window.links[index].value !== values[i]) {
-                    changed++
-                    window.links[index].value = values[i];
+    const data = buildData(linkData)
+    const {nodes, links} = makeSankey(data);
+
+    const fontScale = d3.scaleLinear() .range([20, 40]);
+    fontScale.domain(d3.extent(nodes, function(d) { return d.value }));
+
+    const container = d3.select('#chart');
+    const svg = container.append('svg')
+                         .attr('viewBox', [0, 0, width, height]);
+
+    const nodeG = svg.append("g")
+                     .attr("stroke", "#000");
+
+    const linkG = svg.append("g")
+                     .attr("fill", "none")
+                     .attr("stroke-opacity", 0.5);
+
+    const nodeTextG = svg.append("g")
+                         .attr("font-family", "sans-serif")
+                         .attr("font-size", 10)
+
+    function onClick(d, data) {
+        console.error('click', d, data)
+        const g = document.getElementById("sliders")
+
+        if (g.configTooltip) {
+            g.configTooltip.remove();
+            g.configTooltip = null;
+            return;
+        }
+
+        const linkSet = d.sourceLinks
+        const s = makeSlider(linkSet.reduce((a, c, i) => [...a, {
+            name: c.target.name,
+            value: c.value + (i ? a[i - 1].value : 0),
+            update: (values) => {
+                let changed = 0
+                linkSet.forEach(({index}, i) => {
+                    if (window.links[index].value !== values[i]) {
+                        changed++
+                        window.links[index].value = values[i];
+                    }
+                })
+                if (changed) {
+                    const updateData = makeSankey(buildData(window.links))
+                    update(updateData)
+                    console.error('update', updateData)
                 }
-            })
-            if (changed) {
-                const updateData = makeSankey(buildData(window.links))
-                update(updateData)
-                console.error('update', updateData)
             }
+        }], []), {
+            max: d.value,
+            formatValue: function(b) {
+                return `${this.name}: ${parseFloat(this.value - b)} (${format(100*(parseFloat(this.value) - b)/d.value)}%)`
+            }
+        });
+        g.appendChild(s);
+        g.configTooltip = s;
+    }
+
+    function clearOpacity() {
+        d3.selectAll("rect").style("opacity", NODE_BASE_OPACITY);
+        d3.selectAll(".sankey-link").style("opacity", LINK_BASE_OPACITY);
+        d3.selectAll("text").style("opacity", 1);
+    }
+
+    function update(data) {
+        function updateNodes(nodes) {
+            const allNodes = nodeG
+                .selectAll("rect");
+            
+            allNodes
+                .data(nodes)
+                .join(
+                    enter => enter.append("rect"),
+                    update => update,
+                    exit => exit.remove()
+                )
+                .attr("x", d => d.x0)
+                .attr("y", d => d.y0)
+                .attr("height", d => d.y1 - d.y0)
+                .attr("width", d => d.x1 - d.x0)
+                .attr("fill", d => color(d.name))
+                .on("click", d => onClick(d, data))
+                .on("mouseover", d => {
+                    let name = d.name
+
+                    const hl = data.links
+                                   .filter(l => l.source.name === name || l.target.name === name)
+
+                    console.error(hl);
+                    d3.selectAll(".sankey-link")
+                                   .style("opacity",
+                                          l => l.name in hl ? LINK_FG_OPACITY : LINK_BG_OPACITY)
+                })
+                .on("mouseout", clearOpacity)
+            
+
+            allNodes.enter()
+                .append("title")
+                .text(d=> `${d.name}\n${format(d.value)}`)
+                .style("fill", d => d3.rgb(color(d.name)).darker(1.4))
+            
         }
-    }], []), {
-        max: d.value,
-        formatValue: function(b) {
-            return `${this.name}: ${parseFloat(this.value - b)} (${format(100*(parseFloat(this.value) - b)/d.value)}%)`
+        function updateLinks(links) {
+            const link = linkG
+                .selectAll("g")
+                .data(links, links => `${links.source.name}${links.target.name}`)
+            
+            link.exit().remove()
+
+            link
+                .attr('debug', console.error.bind(console))
+            
+            link.select('path')
+                .attr("d", l => d3.sankeyLinkHorizontal ? d3.sankeyLinkHorizontal()(l) : l.path)
+                .attr("stroke", d => color(d.source.name))
+                .attr("stroke-width", d => Math.max(1, d.width))
+                .style("opacity", LINK_BASE_OPACITY)
+
+            link.select('title')
+                .text(d => `${d.source.name} → ${d.target.name}\n${format(d.value)}`);
+
+            const enter = link.enter()
+                              .append('g')
+                              .attr("class", "sankey-link");
+            enter
+                .append("path")
+                .attr("d", l => d3.sankeyLinkHorizontal ? d3.sankeyLinkHorizontal()(l) : l.path)
+                .attr("stroke", d => color(d.source.name))
+                .attr("stroke-width", d => Math.max(1, d.width))
+                .style("opacity", LINK_BASE_OPACITY);
+            
+            enter.append("title")
+                 .text(d => `${d.source.name} → ${d.target.name}\n${format(d.value)}`);
+
         }
-    });
-    g.appendChild(s);
-    g.configTooltip = s;
+        function updateNodeTexts(nodes) {
+            function wrap(text, width = 30) {
+                text.each(function() {
+                    var text = d3.select(this),
+                        words = text.text().split(/\s+/).reverse(),
+                        word,
+                        line = [],
+                        lineNumber = 0,
+                        lineHeight = 1.1, // ems
+                        y = text.attr("y"),
+                        dy = parseFloat(text.attr("dy")),
+                        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+                    while (word = words.pop()) {
+                        line.push(word);
+                        tspan.text(line.join(" "));
+                        if (tspan.node().getComputedTextLength() > width) {
+                            line.pop();
+                            tspan.text(line.join(" "));
+                            line = [word];
+                            tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+                        }
+                    }
+                });
+            }
+            
+            const node = nodeTextG
+                .selectAll("text")
+                .data(nodes)
+                .join("text")
+                .attr("transform", d => `translate(${d.x0 + 30} 0)`)
+                .attr("y", d => d.y0)
+                .attr("dy", "0.05em")
+                .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
+                .style("fill", d => d3.rgb(color(d.name)).darker(1.4))
+                .style("font-size", d => Math.floor(fontScale(d.value)) + "px")
+                .text(d => d.name)
+                .call(wrap);
+        }
+        
+
+        updateLinks(data.links)
+        updateNodes(data.nodes)
+        updateNodeTexts(data.nodes)
+    }
+
+    update({nodes, links})
 }
 
-function clearOpacity() {
-    d3.selectAll("rect").style("opacity", 0.5);
-    d3.selectAll(".sankey-link").style("opacity", 0.7);
-    d3.selectAll("text").style("opacity", 1);
-}
-
-function update(data) {
-    function updateNodes(nodes) {
-        const allNodes = nodeG
-            .selectAll("rect");
-        
-        allNodes
-            .data(nodes)
-            .join(
-                enter => enter.append("rect"),
-                update => update,
-                exit => exit.remove()
-            )
-            .attr("x", d => d.x0)
-            .attr("y", d => d.y0)
-            .attr("height", d => d.y1 - d.y0)
-            .attr("width", d => d.x1 - d.x0)
-            .attr("fill", d => color(d.name))
-            .on("click", d => onClick(d, data))
-            .on("mouseover", d => {
-                let name = d.name
-                allNodes.style("opacity", d => highlightNodes(d, name))
-                d3.selectAll(".sankey-link")
-                            .style("opacity",
-                                   l => l.source.name === name || l.target.name === name ? 1 : 0.3)
-            })
-            .on("mouseout", clearOpacity)
-        
-
-        allNodes.enter()
-            .append("title")
-            .text(d=> `${d.name}\n${format(d.value)}`)
-            .style("fill", d => d3.rgb(color(d.name)).darker(1.4))
-        
-    }
-    function updateLinks(links) {
-        const link = linkG
-            .selectAll("g")
-            .data(links, links => `${links.source.name}${links.target.name}`)
-        
-        link.exit().remove()
-
-        link
-            .attr('debug', console.error.bind(console))
-        
-        link.select('path')
-            .attr("d", d3.sankeyLinkHorizontal())
-            .attr("stroke", d => color(d.source.name))
-            .attr("stroke-width", d => Math.max(1, d.width));
-
-        link.select('title')
-              .text(d => `${d.source.name} → ${d.target.name}\n${format(d.value)}`);
-
-        const enter = link.enter()
-                          .append('g')
-                          .attr("class", "sankey-link");
-        enter
-            .append("path")
-            .attr("d", d3.sankeyLinkHorizontal())
-            .attr("stroke", d => color(d.source.name))
-            .attr("stroke-width", d => Math.max(1, d.width));
-        
-        enter.append("title")
-             .text(d => `${d.source.name} → ${d.target.name}\n${format(d.value)}`);
-
-    }
-    function updateNodeTexts(nodes) {
-        const node = nodeTextG
-            .selectAll("text")
-            .data(nodes)
-            .join("text")
-            .attr("x", d => (d.x0 < width / 2) ? d.x1 + 6 : d.x0 - 6)
-            .attr("y", d => (d.y1 + d.y0) / 2)
-            .attr("dy", "0.35em")
-            .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
-            .text(d => d.name)
-            .style("fill", d => d3.rgb(color(d.name)).darker(1.4))
-            .style("font-size", d => Math.floor(fontScale(d.value)) + "px")
-    }
-    
-    updateNodes(data.nodes)
-    updateLinks(data.links)
-    updateNodeTexts(data.nodes)
-}
-
-update({nodes, links})
